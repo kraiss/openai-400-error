7291af394afe548045f76240764116ee0ea859a7
Pierrick Rassat <pierrick.rassat@kelkoogroup.com>
2025-03-14 15:58:41 +0100

    ok example + ko example

--- src/main/scala/MainKo.scala
+++ src/main/scala/MainKo.scala
@@ -6,9 +6,10 @@ import com.openai.models.files.FilePurpose
 @main def mainko(): Unit = {
   val client = OpenAIOkHttpClient.builder().apiKey(OpenAIApiKey.apikey).build()
   val batchContent = """{"custom_id": "request-1", "method": "POST", "url": "/v1/chat/completions", "body": {"model": "gpt-3.5-turbo-0125", "messages": [{"role": "system", "content": "You are a helpful assistant."},{"role": "user", "content": "Hello world!"}],"max_tokens": 1000}}"""
+
   val fileCreateParams = FileCreateParams
     .builder()
-    .file(batchContent.getBytes())
+    .file(batchContent.getBytes)
     .purpose(FilePurpose.BATCH)
     .build()
   val fileObject: FileObject = client.files().create(fileCreateParams)
--- src/main/scala/MainOk.scala
+++ src/main/scala/MainOk.scala
@@ -1,17 +1,29 @@
 import com.openai.client.okhttp.OpenAIOkHttpClient
 import com.openai.models.ChatModel
 import com.openai.models.chat.completions.ChatCompletionCreateParams
+import com.openai.models.files.FileCreateParams
+import com.openai.models.files.FileObject
+import com.openai.models.files.FilePurpose
+
+import java.io.File
+import java.io.PrintWriter
+import scala.util.Using
 
 @main def mainok(): Unit = {
   val client = OpenAIOkHttpClient.builder().apiKey(OpenAIApiKey.apikey).build()
+  val batchContent = """{"custom_id": "request-1", "method": "POST", "url": "/v1/chat/completions", "body": {"model": "gpt-3.5-turbo-0125", "messages": [{"role": "system", "content": "You are a helpful assistant."},{"role": "user", "content": "Hello world!"}],"max_tokens": 1000}}"""
 
-  val createParams = ChatCompletionCreateParams.builder
-    .model(ChatModel.GPT_3_5_TURBO)
-    .maxCompletionTokens(512)
-    .addDeveloperMessage("Make sure you mention Stainless!")
-    .addUserMessage("Tell me a story about building the best SDK!")
-    .build
-  client.chat.completions.create(createParams)
-    .choices.stream.flatMap((choice) => choice.message.content.stream)
-    .forEach(System.out.println)
+  val jsonlFile = File.createTempFile("chat_completions", ".jsonl")
+  Using.resource(new PrintWriter(jsonlFile)) { writer =>
+    writer.println(batchContent)
+  }
+
+  val fileCreateParams = FileCreateParams
+    .builder()
+    .file(jsonlFile.toPath)
+    .purpose(FilePurpose.BATCH)
+    .build()
+  val fileObject: FileObject = client.files().create(fileCreateParams)
+
+  println(fileObject)
 }
